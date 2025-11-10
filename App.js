466b48a9e74ceece6b5b39faefd1e9314716a94a
suryavarmacherukuri2327-creator <suryavarmacherukuri2327@gmail.com>
2025-11-10 07:00:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Props from './Components/Props';
+
+function App() {
+
+	return (
+  <div>
+
+    <Props> </Props>
+      
+  </div>
+		
+	);
+}
+
+export default App;
