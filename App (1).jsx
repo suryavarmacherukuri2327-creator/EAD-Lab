4e269f2c91f5c2386653166a8eddebee989aedda
suryavarmacherukuri2327-creator <suryavarmacherukuri2327@gmail.com>
2025-11-10 06:58:50 +0530

    Add files via upload

---
+++ App (1).jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import ApiCalls from './comp1/ApiCalls'
+function App() {
+  
+  return (
+    <>
+      <ApiCalls></ApiCalls>
+    </>
+  )
+}
+
+export default App
